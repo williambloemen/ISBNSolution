08b3e1774d0175b5e3e281fad27801006d39f1cc
williambloemen <williambloemen@hotmail.com>
2017-11-28 13:14:58 +0100

    Add project files.

---
+++ ISBN/Isbn.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISBN
+{
+    public class Isbn
+    {
+        private const long GrootsteGetalMet13_Cijfers = 9999999999999L;
+        private const long KleinsteGetalMet13_Cijers = 1000000000000L;
+        private long nummer;
+
+        public Isbn(long nummer)
+        {
+            if (nummer < KleinsteGetalMet13_Cijers || nummer > GrootsteGetalMet13_Cijfers)
+            {
+                throw new ArgumentException();
+            }
+            var somEvenCijfers = 0L;
+            var somOnevenCijfers = 0L;
+            var teVerwerkenCijfers = nummer / 10;
+
+            for (int teller = 0; teller != 6; teller++)
+            {
+                somEvenCijfers += teVerwerkenCijfers % 10;
+                teVerwerkenCijfers /= 10;
+                somOnevenCijfers += teVerwerkenCijfers % 10;
+                teVerwerkenCijfers /= 10;
+            }
+            var controleGetal = somEvenCijfers * 3 + somOnevenCijfers;
+            var naastGelegenHoger10Tal = controleGetal - controleGetal % 10 + 10;
+            var verschil = naastGelegenHoger10Tal - controleGetal;
+            var laatsteCijfer = nummer % 10;
+            if (verschil == 10)
+            {
+                if (laatsteCijfer != 0)
+                {
+                    throw new ArgumentException();
+                }
+            }
+            else
+            {
+                if (laatsteCijfer != verschil)
+                {
+                    throw new ArgumentException();
+                }
+            }
+            this.nummer = nummer;
+        }
+        public override string ToString()
+        {
+            return nummer.ToString();
+        }
+    }
+}
---
+++ ISNBTest/IsbnTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ISBN;
+
+namespace ISNBTest
+{
+    [TestClass]
+    public class IsbnTest
+    {
+        [TestMethod, ExpectedException(typeof(ArgumentException))]
+        public void HetNummer0IsVerkeerd()
+        {
+            new Isbn(0);
+        }
+        [TestMethod, ExpectedException(typeof(ArgumentException))]
+        public void EenNegatiefNummerIsVerkeerd()
+        {
+            new Isbn(-9789027439642L);
+        }
+        [TestMethod, ExpectedException(typeof(ArgumentException))]
+        public void EenNummerMet12CijfersIsVerkeerd()
+        {
+            new Isbn(978902743964L);
+        }
+        [TestMethod, ExpectedException(typeof(ArgumentException))]
+        public void EenNummerMet14CijfersIsVerkeerd()
+        {
+            new Isbn(97890274396421L);
+        }
+        [TestMethod, ExpectedException(typeof(ArgumentException))]
+        public void EenNummerMet13CijfersMetVerkeerdControleGetal2()
+        {
+            new Isbn(8789027439642L);
+        }
+        [TestMethod]
+        public void EenNummerMet13CijfersMetCorrectControleGetal2()
+        {
+            new Isbn(9789027439642L);
+        }
+        [TestMethod, ExpectedException(typeof(ArgumentException))]
+        public void EenNummerMet13CijfersMetVerkeerdControleGetal0()
+        {
+            new Isbn(7789227439640L);
+        }
+        [TestMethod]
+        public void EenNummerMet13CijfersMetCorrectControleGetal0()
+        {
+            new Isbn(9789227439640L);
+        }
+
+    }
+}
